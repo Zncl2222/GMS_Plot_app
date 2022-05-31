0837d33d0371aec16b82905c75b01c6a3db93b29
Zncl2222 <3002shinning@gmail.com>
2022-05-31 23:38:07 +0800

    fix: fig save dir and class container
    Change fig save dir to the ./static/images/
    Use class self.Z as a data container. Return value is not necessary now.
    Remove self.cc param and some reduntant space and symbol.
    feat class semivariance (not yet finish, it can only plot single plot
    now)

--- model/markovchain_plot.py
+++ model/markovchain_plot.py
@@ -6,49 +6,39 @@ from matplotlib.font_manager import FontProperties
 from pylab import *
 
 
-class GMS_Plot:
+class markov_chain:
     
     def ReadData(self):
         a=os.listdir('./static/data')
         a.sort(key = len)
-        Z = pd.read_table('./static/data'+'\\'+a[0],header=None,encoding='UTF16',sep='\s+')
-        Z = np.zeros((len(a),np.shape(Z)[0],2))
-        for i in range(len(a)):
-            Y=pd.read_table(filepath+'\\'+a[i],header=None,encoding='UTF16',sep='\s+')
-            Z[i,:]=Y.iloc[:,2:4]
-        return Z
-    
-    def ReadData2(self):
-        a=os.listdir('./static/data')
-        a.sort(key = len)
         Z = pd.read_table('./static/data'+'\\'+a[0],header=None,encoding='UTF16')
-        Z = np.zeros((len(a),np.shape(Z)[0],2))
+        self.Z = np.zeros((len(a),np.shape(Z)[0],2))
 
         for i in range(len(a)):
             Y=pd.read_table('./static/data'+'\\'+a[i],header=None,encoding='UTF16')
             Y[2]=Y[2].apply(lambda x: np.NaN if str(x).isspace() else x)
             Y[3]=Y[3].apply(lambda x: np.NaN if str(x).isspace() else x)
-            Z[i,:]=Y.iloc[:,2:4]
-        return Z
+            self.Z[i,:]=Y.iloc[:,2:4]
 
-    def Subplot4(self,Z,ratio):
+        return self.Z
 
-        for i in range(len(Z[0][:,0])):
-            if abs(Z[0][i,0]-Z[0][i-1,0])>=20:
+    def Subplot4(self, ratio):
+
+        for i in range(len(self.Z[0][:,0])):
+            if abs(self.Z[0][i,0]-self.Z[0][i-1,0])>=20:
                 SL=i
-            if np.isnan(Z[0][i,0])==True:
+            if np.isnan(self.Z[0][i,0])==True:
                 SL2=i
                 break
                 
         fig, axes = plt.subplots(4,4, sharex=True, sharey=True,figsize=(15,8))
-        print(axes.flatten())
         x1=range(SL)
         x2=range(SL,SL2)
-        x_max1=Z[0][SL2-1][0]
+        x_max1=self.Z[0][SL2-1][0]
         count=0
         for i, ax in enumerate(axes.flatten()):
-            ax.plot(Z[count][x1,0], Z[count][x1,1],linewidth=2,color='k')
-            ax.plot(Z[count][x2,0], Z[count][x2,1],linewidth=2,color='r',linestyle='--')
+            ax.plot(self.Z[count][x1,0], self.Z[count][x1,1],linewidth=2,color='k')
+            ax.plot(self.Z[count][x2,0], self.Z[count][x2,1],linewidth=2,color='r',linestyle='--')
             ax.set_ylim([0, 1])
             
             ax.set_xlim([0, x_max1*ratio])
@@ -57,30 +47,29 @@ class GMS_Plot:
             plt.setp(ax.get_yticklabels(), fontsize=16)
             count+=1
         plt.tight_layout()
-        #plt.savefig('./static/images/temp.png',dpi=800)
         plt.savefig('./static/images/GMSPlot.png',dpi=800)
 
-    def Subplot3(self,Z,ratio):
+    def Subplot3(self, ratio):
 
-        for i in range(len(Z[0][:,0])):
-            if abs(Z[0][i,0]-Z[0][i-1,0])>=20:
+        for i in range(len(self.Z[0][:,0])):
+            if abs(self.Z[0][i,0]-self.Z[0][i-1,0])>=20:
                 SL=i
-            if np.isnan(Z[0][i,0])==True:
+            if np.isnan(self.Z[0][i,0])==True:
                 SL2=i
                 break
 
         fig, axes = plt.subplots(3,3, sharex=True, sharey=True,figsize=(15,8))
-        print(axes.flatten())
+
         x1=range(SL)
         x2=range(SL,SL2)
-        x_max1=Z[0][SL2-1][0]
+        x_max1=self.Z[0][SL2-1][0]
         count=0
         for i, ax in enumerate(axes.flatten()):
-            ax.plot(Z[count][x1,0], Z[count][x1,1],linewidth=2,color='k')
-            ax.plot(Z[count][x2,0], Z[count][x2,1],linewidth=2,color='r',linestyle='--')
+            ax.plot(self.Z[count][x1,0], self.Z[count][x1,1],linewidth=2,color='k')
+            ax.plot(self.Z[count][x2,0], self.Z[count][x2,1],linewidth=2,color='r',linestyle='--')
             ax.set_ylim([0, 1])
-            if self.cc==False:
-                ax.set_xlim([0, x_max1*ratio])
+        
+            ax.set_xlim([0, x_max1*ratio])
             ax.yaxis.set_major_formatter(FormatStrFormatter('%1.1f'))
             plt.setp(ax.get_xticklabels(), fontsize=16)
             plt.setp(ax.get_yticklabels(), fontsize=16)
@@ -88,30 +77,28 @@ class GMS_Plot:
         plt.tight_layout()
         plt.savefig('./static/images/GMSPlot.png',dpi=800)
 
+    def Subplot2(self, ratio):
 
-    def Subplot2(self,Z,ratio):
-
-        for i in range(len(Z[0][:,0])):
-            if abs(Z[0][i,0]-Z[0][i-1,0])>=20:
+        for i in range(len(self.Z[0][:,0])):
+            if abs(self.Z[0][i,0]-self.Z[0][i-1,0])>=20:
                 SL=i
-            if np.isnan(Z[0][i,0])==True:
+            if np.isnan(self.Z[0][i,0])==True:
                 SL2=i
                 break
         
         fig, axes = plt.subplots(2,2, sharex=True, sharey=True,figsize=(15,8))
 
-
         x1=range(SL)
         x2=range(SL,SL2)
-        x_max1=Z[0][SL2-1][0]
+        x_max1=self.Z[0][SL2-1][0]
         count=0
 
         for i, ax in enumerate(axes.flatten()):
-            ax.plot(Z[count][x1,0]*ratio, Z[count][x1,1],linewidth=2,color='k')
-            ax.plot(Z[count][x2,0]*ratio, Z[count][x2,1],linewidth=2,color='r',linestyle='--')
+            ax.plot(self.Z[count][x1,0]*ratio, self.Z[count][x1,1],linewidth=2,color='k')
+            ax.plot(self.Z[count][x2,0]*ratio, self.Z[count][x2,1],linewidth=2,color='r',linestyle='--')
             ax.set_ylim([0, 1])
-            if self.cc==False:
-                ax.set_xlim([0, x_max1*ratio])
+            
+            ax.set_xlim([0, x_max1*ratio])
             ax.yaxis.set_major_formatter(FormatStrFormatter('%1.1f'))
             plt.setp(ax.get_xticklabels(), fontsize=16)
             plt.setp(ax.get_yticklabels(), fontsize=16)
@@ -121,5 +108,32 @@ class GMS_Plot:
         
         plt.savefig('./static/images/GMSPlot.png',dpi=800)
         
-        if __name__=='__main__':
-            plt.show(block=False)
+
+class semivariance(markov_chain):
+
+    def semi_plot(self,ratio):
+
+        for i in range(len(self.Z[0][:,0])):
+            if np.isnan(self.Z[0][i,0])==True and np.isnan(self.Z[0][i-1,0])==False:
+                SL=i
+            if np.isnan(self.Z[0][i,0])==True and np.isnan(self.Z[0][i+1,0])==False:
+                SL2=i
+                break
+        x1=range(SL)
+        x2=range(SL2,len(self.Z[0][:]))
+
+        for i in range(len(self.Z)):
+            plt.figure(num=123+i,figsize=(9,7))
+            plt.plot(self.Z[i][x1,0]*ratio, self.Z[i][x1,1],linewidth=2,color='k',label="Model")
+            plt.plot(self.Z[i][x2,0]*ratio, self.Z[i][x2,1],linewidth=2,color='r',linestyle='--',label="Theory")
+            plt.xticks(fontsize=18);plt.yticks(fontsize=18)
+            plt.legend()
+            plt.ylabel("Semi-variance",fontsize=24)
+            plt.xlabel("Lag distance",fontsize=24)
+            plt.ticklabel_format(style='sci',axis='x',scilimits=(0,0),useMathText=True,useOffset=False)
+            plt.rc('font', size=17)
+            plt.grid(alpha=0.25)
+            plt.tight_layout()
+            plt.savefig('./static/images/semivariance.png',dpi=500)
+
+        
