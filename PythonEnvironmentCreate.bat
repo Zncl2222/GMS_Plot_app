86fee49cc6f3d07f7cca19f949989866bde90744
Zncl2222 <3002shinning@gmail.com>
2022-06-01 13:32:05 +0800

    initial commit

---
+++ PythonEnvironmentCreate.bat
@@ -0,0 +1,7 @@
+call C:\Users\3002shinning\anaconda3\Scripts\activate.bat
+conda create --name GMS_Plot_App
+pip install pandas
+pip install matplotlib
+pip install numpy
+pip install flask
+pip install werkzeug
---
+++ PythonEnvironmentInstall.bat
@@ -0,0 +1,6 @@
+call C:\Users\%username%\anaconda3\Scripts\activate.bat C:\Users\%username%\anaconda3\envs\GMS_Plot_App
+pip install pandas
+pip install matplotlib
+pip install numpy
+pip install flask
+pip install werkzeug
