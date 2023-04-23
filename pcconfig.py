2b46b2b35688038bb855768c01e684826c182fb1
Zncl2222 <3002shinning@gmail.com>
2023-04-23 16:22:29 +0800

    build(docker): add Dockerfile and env_example

--- pcconfig.py
+++ pcconfig.py
@@ -7,12 +7,17 @@ env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
 
 api_url = os.getenv('API_URL')
+frontend_port = os.getenv('FRONTEND_PORT')
 backend_port = os.getenv('BACKEND_PORT')
+mode = os.getenv('MODE')
+
+bun_path = '/app/.bun/bin/bun' if mode == 'PROD' else pc.constants.BUN_PATH
 
 config = pc.Config(
     app_name='plot_gms',
     api_url=api_url,
+    port=frontend_port,
+    bun_path=bun_path,
     backend_port=backend_port,
     db_url='sqlite:///pynecone.db',
-    env=pc.Env.DEV,
 )
